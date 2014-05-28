ed9d07af1d43e2e6841b4b698cc13fa1ed2e8302
georgevella <admin@georgevella.com>
2014-05-28 10:14:54 +0200

    Initial implementation of Order creation controller

--- Food/Controllers/OrderController.cs
+++ Food/Controllers/OrderController.cs
@@ -1,17 +1,98 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using FoodOrder.DataAccess;
+using FoodOrder.DataAccess.Model;
 
 namespace FoodOrder.Controllers
 {
     public class OrderController : BaseController
     {
+        private readonly IDataAccessLayer _dataAccessLayer;
+        // GET: Order
         public OrderController(IDataAccessLayer dataAccessLayer) : base(dataAccessLayer)
         {
+            _dataAccessLayer = dataAccessLayer;
         }
 
-        public ActionResult New()
+        public ActionResult Index()
         {
-            throw new System.NotImplementedException();
+            return View();
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Order/Create
+        public ActionResult Create()
+        {
+            ViewBag.Stores = _dataAccessLayer.GetRepositoryFor<Store>();
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
